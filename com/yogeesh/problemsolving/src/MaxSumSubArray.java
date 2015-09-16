f40b0a7ee5379bfdc81aab810f10c341ba805c89
Yogeesh R <yogeesh.srkvs@gmail.com>
2015-09-16 22:02:06 +0530

    change logic
    change logic

--- com/yogeesh/problemsolving/src/MaxSumSubArray.java
+++ com/yogeesh/problemsolving/src/MaxSumSubArray.java
@@ -8,35 +8,25 @@ public class MaxSumSubArray {
 	
 	public static void main(String []args) {
 		
-		int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3}, sum=0, start=0, end=0, maxsum=0, tempStrt=0;
+		int arr[] = {-2000, -1, 2000, -4000}, sum=0, start=0, end=0, maxsum=0, tempStrt=0;
 		
 		for (int index=0; index < arr.length; index++) {
 			
-			//handle when we get negative numbers
-			if (arr[index]<0) {
-				if (maxsum<sum) {
-					start=tempStrt;
-					end=index-1;
-					maxsum=sum;
-				}
+			sum = sum + arr[index];
+			
+			if (sum<0) {
 				sum=0;
 				tempStrt=index+1;
-				continue;
 			}
 			
-			if (sum+arr[index] > sum) {
-				sum = sum + arr[index];
-			} 
+			if (maxsum < sum) {
+				maxsum=sum;
+				start=tempStrt;
+				end=index;
+			}
 			
 		}
-		
-		//this is where last sub sequence results to the max array
-		if (sum > maxsum) {
-			start=tempStrt;
-			end=arr.length-1;
-			maxsum=sum;
-		}
-		
+				
 		System.out.println("Max Sub Array is : ");
 		
 		for (int j=start; j<=end; j++) {
